6cb7495a4512afa5e0fb8d7edea358aeb23f5a82
Raslan <raslan@Raslan-MacBook-Pro.local>
2023-02-03 15:26:12 +0200

    Get country info by date

--- covid19/views/api/top_countries.py
+++ covid19/views/api/top_countries.py
@@ -1,7 +1,7 @@
 # pylint: disable=import-error
 import logging
+from datetime import datetime, timedelta
 
-from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.views import APIView
@@ -19,6 +19,10 @@ class TopCountries(APIView):
         user_id = request.user.id
         status = request.GET.get('status', '')
         limit = int(request.GET.get('limit', 3))
+        date = request.GET.get('date', '')
+        date_before = datetime.fromisoformat(date) - timedelta(days=1)
+        date_before = date_before.strftime("%Y-%m-%dT00:00:00Z")
+        date_str = datetime.fromisoformat(date).strftime("%Y-%m-%dT00:00:00Z")
 
         logger.info(f'Getting top {limit} countries for user {user_id}')
 
@@ -26,9 +30,18 @@ class TopCountries(APIView):
         countries = user_setting.countries.all()
         all_country_stats = []
         for country in countries:
-            country_stats = CountryDailyStats.objects.filter(
-                country=country.id).latest('date')
-            all_country_stats.append(model_to_dict(country_stats))
+            day_before_stats = CountryDailyStats.objects.get(
+                country=country.id, date=date_before)
+            country_stats = CountryDailyStats.objects.get(
+                country=country.id, date=date_str)
+            all_country_stats.append({
+                "country": country.name,
+                "date": date_str,
+                "confirmed": country_stats.confirmed - day_before_stats.confirmed,
+                "deaths": country_stats.deaths - day_before_stats.deaths,
+                "recovered": country_stats.recovered - day_before_stats.recovered,
+                "active": country_stats.active - day_before_stats.active
+            })
 
         all_country_stats.sort(key=lambda x: x[status], reverse=True)
 
--- covid19/views/api/user_statistics.py
+++ covid19/views/api/user_statistics.py
@@ -1,7 +1,7 @@
 # pylint: disable=import-error
 import logging
+from datetime import datetime, timedelta
 
-from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.views import APIView
@@ -11,12 +11,17 @@ from covid19.models.user_setting import UserSetting
 
 logger = logging.getLogger('django')
 
+
 class UserStats(APIView):
 
     @csrf_exempt
     def get(self, request):
         user_id = request.user.id
+        date = request.GET.get('date', '')
 
+        date_before = datetime.fromisoformat(date) - timedelta(days=1)
+        date_before = date_before.strftime("%Y-%m-%dT00:00:00Z")
+        date_str = datetime.fromisoformat(date).strftime("%Y-%m-%dT00:00:00Z")
         logger.info(f'Getting countries summary for user {user_id}')
 
         user_setting = UserSetting.objects.get(user_id=user_id)
@@ -24,8 +29,17 @@ class UserStats(APIView):
 
         all_country_stats = []
         for country in countries:
-            country_stats = CountryDailyStats.objects.filter(
-                country=country).latest('date')
-            all_country_stats.append(model_to_dict(country_stats))
+            day_before_stats = CountryDailyStats.objects.get(
+                country=country.id, date=date_before)
+            country_stats = CountryDailyStats.objects.get(
+                country=country.id, date=date_str)
+            all_country_stats.append({
+                "country": country.name,
+                "date": date_str,
+                "confirmed": country_stats.confirmed - day_before_stats.confirmed,
+                "deaths": country_stats.deaths - day_before_stats.deaths,
+                "recovered": country_stats.recovered - day_before_stats.recovered,
+                "active": country_stats.active - day_before_stats.active
+            })
 
         return JsonResponse({'result': all_country_stats})
